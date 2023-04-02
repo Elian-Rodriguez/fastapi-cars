1219e9ee0c37a51cfb73f680535cefb005ed65e6
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-04-02 00:14:26 -0500

    Modificacion del Middleware  para no exponer las sentensian de insert en los logs ni en la respuesta json

--- middlewares/error_handler.py
+++ middlewares/error_handler.py
@@ -1,6 +1,8 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
+
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
@@ -9,5 +11,9 @@ class ErrorHandler(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
         try:
             return await call_next(request)
+        except IntegrityError as e:
+            if "UNIQUE constraint failed" in str(e):
+                return JSONResponse(status_code=400, content={'error': 'Valor del Id duplicado'})
+            return JSONResponse(status_code=500, content={'error': 'Error en la base de datos'})
         except Exception as e:
             return JSONResponse(status_code=500, content={'error': str(e)})
