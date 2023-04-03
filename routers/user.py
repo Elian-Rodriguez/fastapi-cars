58aca82a2191bef4578991e7c62facb6094aa159
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2023-04-03 18:59:09 -0500

    Update user.py

--- routers/user.py
+++ routers/user.py
@@ -9,6 +9,9 @@ user_router = APIRouter()
 
 @user_router.post('/login', tags=['auth'])
 def login(user: User):
+    """
+       Se requiere autenticación JWT para acceder a esta ruta. El usuario para la autenticación es admin@gmail.com con contraseña admin.
+    """
     if user.email == "admin@gmail.com" and user.password == "admin":
         token: str = create_token(user.dict())
         return JSONResponse(status_code=200, content=token)
